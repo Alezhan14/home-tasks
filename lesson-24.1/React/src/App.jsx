1c7d94ed5be14a815aabfe5479fb10c6cefcbd5f
Oleh.P <oleg.poruchikov@gmail.com>
2025-06-04 10:16:36 +0300

    HT 24.1 upd

--- lesson-24.1/React/src/App.jsx
+++ lesson-24.1/React/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header.jsx";
+import List from "./components/List.jsx";
 
 
 function App() {
     return (
         <>
+            <div className="App">
+                <Header />
+                <List />
+            </div>
         </>
     )
 }
--- lesson-24.1/React/src/components/NavItem.jsx
+++ lesson-24.1/React/src/components/NavItem.jsx
@@ -1,7 +1,7 @@
 function NavItem(props) {
     return (
         <>
-            <li className="nav-item">
+            <li className="nav-item btn">
                 <a className="nav-link active" aria-current="page" href={props.link}>{props.text}</a>
             </li>
         </>
